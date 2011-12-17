19986882b4edd8c8b6eebc265c98dcc99828863f
Kyle McGowan <kyle@Pegasus.(none)>
2011-12-17 18:34:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotePro::Application.config.secret_token = 'e07df8fda35af9636dcf2c0d1c37f99f01ef186906f9a2a697eb0e010511b5665cb1894ac007b86b067030305530b0ae7a09bed6c9a17b2a9d452206c1159969'
